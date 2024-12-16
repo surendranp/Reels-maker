e024640a136108299cde8ba809cd887337eb5c06
suren67 <surendranpandi02@gmail.com>
2024-12-16 13:36:42 +0530

    first commit23

--- server/server.js
+++ server/server.js
@@ -9,7 +9,7 @@ const multer = require('multer');  // Add multer for file uploads
 require('dotenv').config();
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 
 app.use(cors());
 app.use(bodyParser.json({ limit: '50mb' }));
